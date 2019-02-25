8d61e6d7575ed47b71334067ef37feb82ec7a8e3
Alumno <jesus7_sg@hotmail.com>
2019-02-25 13:55:12 +0100

    j

--- Jesus_Sanchez_Goñi/PHP/EJERCICIOS-CLASSROOM/login.php
+++ Jesus_Sanchez_Goñi/PHP/EJERCICIOS-CLASSROOM/login.php
@@ -7,7 +7,19 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 </head>
 <body>
-    
+<form name="formulario" method="post" action="almacenar.php">
+        <p>
+            <label for="Email">Email:</label>
+            <input type="text" name="Email" id="Email" value="<?php  if (isset($_COOKIE['mailUsu'])) { echo $_COOKIE['mailUsu']; ?>"/>
+                
+        </p>
+        <p>
+            <label for="Passwprd">Password:</label>
+            <input type="password" name="Password" id="Password"/>
+        </p>
+        <input type="checkbox" name="¿Recordar email?">
+        <input type="submit" value="Enviar formulario">
+        </form>
     
 </body>
 </html>
